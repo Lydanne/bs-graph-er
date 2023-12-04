cd77a19d867a29ad2d782749a8e3d6ae9ae2eec2
WumaCoder <15804854160@163.com>
2023-12-04 13:48:30 +0800

    chore: 静态打包

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@ const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
+  // i18n,
+  output: 'export',
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
+
+  distDir: 'release',
+
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,14 @@ const DynamicComponentWithNoSSR = dynamic(() => import("@/views/Home"), {
   ssr: false,
 });
 
-export async function getStaticProps({ locale }: any) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["common"])),
-      // Will be passed to the page component as props
-    },
-  };
-}
+// export async function getStaticProps({ locale }: any) {
+//   return {
+//     props: {
+//       ...(await serverSideTranslations(locale, ["common"])),
+//       // Will be passed to the page component as props
+//     },
+//   };
+// }
 
 const Home: NextPage = () => {
   return (
