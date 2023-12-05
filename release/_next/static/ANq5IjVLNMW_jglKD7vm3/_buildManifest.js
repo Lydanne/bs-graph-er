9f14fc47eca6e520c156cf73dc1b121a7683afd4
WumaCoder <1829913225@qq.com>
2023-12-05 14:04:23 +0800

    chore: publish

--- release/_next/static/ANq5IjVLNMW_jglKD7vm3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-80ac5e64665f44b4.js"],"/_error":["static/chunks/pages/_error-6934fcdb04353ec6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ release/_next/static/m7gvbzT5_n79sFHwIMi9H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-474d94e70e3afb7e.js"],"/_error":["static/chunks/pages/_error-6934fcdb04353ec6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
