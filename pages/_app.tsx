26c49202e5a03a566078d534b9b60896f909c136
周寰 <terrycarter1985@gmail.com>
2024-03-10 19:27:36 +0800

    bugfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import "reset-css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { appWithTranslation } from "next-i18next";
 import "@/i18n";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-// export default appWithTranslation(MyApp);
